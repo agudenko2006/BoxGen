3d2aeafc516ba7c020b4807f22f6f49bc255dde2
Alexander Gudenko <agudenko2006@gmail.com>
2022-02-26 14:27:10 +0300

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install --yes python3 python3-pip
+pip3 install aiogram
+
+echo "Enter your Telegram Bot Api token"
+read token
+echo "Enter your printer's name"
+read printer
+
+echo "export TOKEN=\"$token\"">>~/.bashrc
+echo "export PRINTER=\"$printer\"">>~/.bashrc
+
+echo "Now you can run \"python3 bot.py\" to start the bot."
