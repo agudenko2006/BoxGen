ac5ce30138f1b6e6c5a9b821836d7be24dbf822a
Alexander Gudenko <agudenko2006@gmail.com>
2022-03-10 14:11:12 +0300

    Fixed cups install

--- setup.sh
+++ setup.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install --yes python3 python3-pip
+sudo apt install --yes python3 python3-pip cron cups
 pip3 install aiogram
+
+sudo usermod -aG $USER lp
+sudo usermod -aG $USER lpprint
+sudo systemctl restart cups
+
+sudo cp .cupsd.conf /etc/cups/cupsd.conf
+echo "Open $HOSTNAME:631 on another computer, set your printer up and press RETURN"
+read >> /dev/null
+echo "Enter your printer's name"
+read printer
 
 echo "Enter your Telegram Bot Api token"
 read token
-echo "Enter your printer's name"
-read printer
 
 echo "export TOKEN=\"$token\"">>~/.bashrc
 echo "export PRINTER=\"$printer\"">>~/.bashrc
 
-source ~/.bashrc
+export TOKEN=\"$token\"
+export PRINTER=\"$printer\"
+
 echo "Now you can run \"python3 bot.py\" to start the bot."
